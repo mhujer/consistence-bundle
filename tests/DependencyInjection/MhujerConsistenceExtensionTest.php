7ba9feb4e3ac9c24b844e5fc39ac4ca1cfd08433
Martin Hujer <mail@martinhujer.cz>
2019-11-06 09:22:53 +0100

    test MhujerConsistenceExtension

---
+++ tests/DependencyInjection/MhujerConsistenceExtensionTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace Mhujer\ConsistenceBundle\DependencyInjection;
+
+use Mhujer\ConsistenceBundle\Enum\EnumTranslator;
+use Mhujer\ConsistenceBundle\Twig\EnumTranslatorExtension;
+use Mhujer\ConsistenceBundle\Twig\EnumTranslatorRuntime;
+
+class MhujerConsistenceExtensionTest extends \Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase
+{
+
+    /**
+     * @return \Symfony\Component\DependencyInjection\Extension\ExtensionInterface[]
+     */
+    protected function getContainerExtensions(): array
+    {
+        return [
+            new MhujerConsistenceExtension(),
+        ];
+    }
+
+    public function testServicesAreRegistered(): void
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasService('mhujer_consistence.enum.enum_translator', EnumTranslator::class);
+        $this->assertContainerBuilderHasService('Mhujer\ConsistenceBundle\Enum\EnumTranslator', EnumTranslator::class);
+
+        $this->assertContainerBuilderHasService('mhujer_consistence.twig.enum_translator_extension', EnumTranslatorExtension::class);
+        $this->assertContainerBuilderHasServiceDefinitionWithTag('mhujer_consistence.twig.enum_translator_extension', 'twig.extension');
+
+        $this->assertContainerBuilderHasService('mhujer_consistence.twig.enum_translator_runtime', EnumTranslatorRuntime::class);
+        $this->assertContainerBuilderHasServiceDefinitionWithTag('mhujer_consistence.twig.enum_translator_runtime', 'twig.runtime');
+    }
+
+}
